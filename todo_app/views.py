3159b06d87c2eacedc3b5912bafddfceb876c2ef
nsdNite <nsd7night@gmail.com>
2023-12-20 21:45:19 +0200

    Update index view, index template

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.views import generic, View
 
+from todo_app.models import Task
+
 
 class IndexView(View):
-    template_name = "todo_app/index.html"
+    model = Task
+    template_name = "todo_app/"
+    context_object_name = "task_list"
+
--- todo_list_site/settings.py
+++ todo_list_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-b-z07cspxp_tum@#5e_886&((p+dodpl@uw=my1b46*vio77@3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1:8000"]
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "bootstrap5"
+    "bootstrap5",
     "todo_app",
 ]
 
